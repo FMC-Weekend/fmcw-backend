07d54be07d790d9aeb83acdd5742cc0781f7a0cf
Shubham Kumar <98862179+shubham21155102@users.noreply.github.com>
2022-12-19 03:27:00 +0530

    Updated authentication

--- middleware/admin_auth.js
+++ middleware/admin_auth.js
@@ -1,6 +1,6 @@
 const jwt=require('jsonwebtoken');
 const userModel=require('../models/admin_user');
-const auth=async (req,res,next)=>{
+const admin_auth=async (req,res,next)=>{
     try{
         const token=req.cookies.jwt;
         const verifyUser=jwt.verify(token,process.env.SECRET_KEY);

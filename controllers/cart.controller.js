9a9e386b17fe291a7d80501e1653a27ca1fbcf26
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-10-14 04:45:40 +0530

    add delete cart bug resolve

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -2,23 +2,35 @@ const CartModel = require('../models/cart_m');
 const UserModel = require('../models/User_m');
 
 exports.addElementToCart = async (req, res) => {
+    //  console.log(req.body)
+    var { userID, cartItem } = req.body;
+
+
+
     try {
-        const {userID, cartItem} = req.body;
+
         cartItem.payment = {
             status: -1,
             paymentID: "",
             paymentRequestID: ""
         }
+        cartItem.desc = cartItem.description;
+        cartItem.title = cartItem.Title;
+        delete cartItem.Title;
+        delete cartItem.description;
         cartItem.Type = cartItem.type;
         delete cartItem.type;
+        // console.log("userId", userID)
         const cart = await CartModel.findOne({ forUser: userID });
+        // console.log("cart", cart);
+        // console.log("frontend Carts", cartItem);
         cart.cartItems.push(cartItem);
         await cart.save();
-        res.json({
-            status: 'Success'
+        res.status(200).json({
+            status: 'success'
         })
     }
-    catch(err) {
+    catch (err) {
         console.log(err)
         res.json({
             status: 'Failure',
@@ -28,11 +40,23 @@ exports.addElementToCart = async (req, res) => {
 }
 
 exports.deleteElementFromCart = async (req, res) => {
+    console.log(req.body)
     try {
-        const {userID, itemId} = req.body;
-        const cart = await CartModel.findOne({ forUser : userID });
+        const { userID, itemId } = req.body;
+        const cart = await CartModel.findOne({ forUser: userID });
+        // console.log(cart)
+        // cart.cartItems.map(item=>{
+        //     // console.log(item.id)
+        //     if(item.id == itemId){
+        //         console.log("item found")
+        //     }
+        //     else{
+
+        //     }
+
+        // })
         let newCartItems = cart.cartItems.filter(item => {
-            return item._id != itemId
+            return item.id != itemId
         });
         cart.cartItems = newCartItems;
         await cart.save();
@@ -40,7 +64,7 @@ exports.deleteElementFromCart = async (req, res) => {
             status: 'Success'
         })
     }
-    catch(err) {
+    catch (err) {
         res.json({
             status: 'Failure',
             error: err

3d917b73d71f44e77f262763c04940508c4234e3
the-dg04 <devgup04@gmail.com>
2023-10-16 02:51:19 +0530

    added route to delete user carts

--- routers/cart.router.js
+++ routers/cart.router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { addElementToCart, deleteElementFromCart, getCartItems } = require('../controllers/cart.controller');
+const { addElementToCart, deleteElementFromCart, getCartItems, clearCart } = require('../controllers/cart.controller');
 router.route('/cart').post(addElementToCart).delete(deleteElementFromCart).get(getCartItems);
-router.route('/carts').post(getCartItems);
+router.route('/carts').post(getCartItems).delete(clearCart);
 module.exports = router;

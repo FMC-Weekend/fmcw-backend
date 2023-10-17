7c3503bb9438f436337c522dffa3778c9c25c073
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-10-17 11:35:32 +0530

    no deletion after purchase

--- routers/cart.router.js
+++ routers/cart.router.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
-const { addElementToCart, deleteElementFromCart, getCartItems, clearCart } = require('../controllers/cart.controller');
+const { addElementToCart, deleteElementFromCart, getCartItems, clearCart,updatePaymentStatus,pendingPayment,successPayment } = require('../controllers/cart.controller');
 router.route('/cart').post(addElementToCart).delete(deleteElementFromCart).get(getCartItems);
 router.route('/carts').post(getCartItems).delete(clearCart);
+router.route('/payment').patch(updatePaymentStatus);
+router.route('/pending').post(pendingPayment);
+router.route('/success').post(successPayment);
+
 module.exports = router;
